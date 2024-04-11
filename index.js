843f15a2230928b8af89745bd6f42aceb793975a
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-11 04:56:32 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-/**
-//════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                            //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
-//                                                                                            // 
-//                                         Ｖ：2．5．0                                         // 
-//                                                                                            // 
-//                                                                                            // 
-//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
-//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
-//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
-//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
-//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
-//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
-//                                                                                            //
-//                                                                                            //
-//                                                                                            //
-//                                                                                            //
-//════════════════════════════════════════════════════════════════════════════════════════════//
-*                                                                 
-  * @project_name : Gifted-Md
-   * @author : Gifted Tech Info
-   * @youtube : https://www.youtube.com/@giftedtechnexus
-   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
-   * @version 2.5.0
-*
-* 
-   * Created By Gifted Tech Info.
-   * © 2024 Gifted-Md.
+const bot = require(__dirname + '/lib/smd')
+const { VERSION } = require(__dirname + '/config')
 
-
-*/
-
-const _0x24153f=_0x23b1;(function(_0x3431ba,_0x2fbbad){const _0x46be55=_0x23b1,_0x4f56a0=_0x3431ba();while(!![]){try{const _0x1de74a=parseInt(_0x46be55(0x87))/0x1*(parseInt(_0x46be55(0x8b))/0x2)+-parseInt(_0x46be55(0x80))/0x3+parseInt(_0x46be55(0x7c))/0x4*(parseInt(_0x46be55(0x84))/0x5)+parseInt(_0x46be55(0x89))/0x6*(parseInt(_0x46be55(0x81))/0x7)+-parseInt(_0x46be55(0x8d))/0x8+-parseInt(_0x46be55(0x85))/0x9+parseInt(_0x46be55(0x7e))/0xa;if(_0x1de74a===_0x2fbbad)break;else _0x4f56a0['push'](_0x4f56a0['shift']());}catch(_0x3d88cd){_0x4f56a0['push'](_0x4f56a0['shift']());}}}(_0x2ec1,0x3e42f));const bot=require(__dirname+'/lib/smd'),{VERSION}=require(__dirname+_0x24153f(0x86)),start=async()=>{const _0x88748a=_0x24153f;Debug[_0x88748a(0x8c)](_0x88748a(0x7f)+VERSION);try{await bot[_0x88748a(0x88)](),bot[_0x88748a(0x83)][_0x88748a(0x8c)]('⏳\x20Gifted\x20Database\x20is\x20syncing!'),await bot[_0x88748a(0x7d)][_0x88748a(0x82)](),await bot['connect']();}catch(_0x5acb26){Debug[_0x88748a(0x8a)](_0x5acb26),start();}};start();function _0x23b1(_0x7897fc,_0x5a3b18){const _0x2ec1e7=_0x2ec1();return _0x23b1=function(_0x23b1e6,_0x46f7af){_0x23b1e6=_0x23b1e6-0x7c;let _0x3b74df=_0x2ec1e7[_0x23b1e6];return _0x3b74df;},_0x23b1(_0x7897fc,_0x5a3b18);}function _0x2ec1(){const _0x2fdb8e=['48562NGfqUP','info','1667304WpoOTb','4QDScNO','DATABASE','4122290vDaxTu','Gifted\x20','791835xMxEoT','7gCOGSV','sync','logger','2085465RrNnbZ','1744101SAhfAL','/config','1fRKwpL','init','405402dZNvbi','error'];_0x2ec1=function(){return _0x2fdb8e;};return _0x2ec1();}
-
-
-
-
-
-
-
+const start = async () => {
+    Debug.info(`Gifted ${VERSION}`)
+  try {
+    await bot.init()
+    bot.logger.info('⏳ Gifted Database is syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
 
 
