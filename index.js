a84efa2dcaa8e424199f19bc3412f461cecc191b
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-11 04:53:46 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

--- index.js
+++ index.js
@@ -31,28 +31,10 @@
 
 */
 
+const _0x32dc13=_0xa04e;(function(_0x2dce59,_0x3482be){const _0x20018e=_0xa04e,_0xf3e823=_0x2dce59();while(!![]){try{const _0x9458c2=-parseInt(_0x20018e(0x198))/0x1*(-parseInt(_0x20018e(0x199))/0x2)+-parseInt(_0x20018e(0x193))/0x3+parseInt(_0x20018e(0x19c))/0x4+parseInt(_0x20018e(0x192))/0x5*(parseInt(_0x20018e(0x194))/0x6)+-parseInt(_0x20018e(0x19a))/0x7*(-parseInt(_0x20018e(0x1a2))/0x8)+-parseInt(_0x20018e(0x19f))/0x9+-parseInt(_0x20018e(0x1a1))/0xa*(-parseInt(_0x20018e(0x19d))/0xb);if(_0x9458c2===_0x3482be)break;else _0xf3e823['push'](_0xf3e823['shift']());}catch(_0x244bc0){_0xf3e823['push'](_0xf3e823['shift']());}}}(_0x4694,0xcd63c));const bot=require(__dirname+_0x32dc13(0x195)),{VERSION}=require(__dirname+_0x32dc13(0x197)),start=async()=>{const _0xe16f29=_0x32dc13;Debug['info'](_0xe16f29(0x1a0)+VERSION);try{await bot['init'](),bot[_0xe16f29(0x19e)][_0xe16f29(0x196)](_0xe16f29(0x19b)),await bot['DATABASE'][_0xe16f29(0x1a4)](),await bot[_0xe16f29(0x1a3)]();}catch(_0x4b4e22){Debug[_0xe16f29(0x1a5)](_0x4b4e22),start();}};function _0xa04e(_0x1c2a04,_0x574571){const _0x469480=_0x4694();return _0xa04e=function(_0xa04e13,_0x4c0420){_0xa04e13=_0xa04e13-0x192;let _0x5c1a2b=_0x469480[_0xa04e13];return _0x5c1a2b;},_0xa04e(_0x1c2a04,_0x574571);}start();function _0x4694(){const _0x51981e=['sync','error','8001425lrEPpc','3965850wlwNlg','6xzlccl','/lib/smd','info','/config','1IepPfC','1242086LvJdBx','3702958mOfPBp','⏳\x20Gifted\x20Database\x20is\x20syncing!','1292172HNgBzd','39490bEAcpG','logger','11713041PRUVQr','Gifted\x20','1090DxaaUG','8SDUUpL','connect'];_0x4694=function(){return _0x51981e;};return _0x4694();}
 
 
 
-
-
-
-const bot = require(__dirname + '/lib/smd')
-const { VERSION } = require(__dirname + '/config')
-
-const start = async () => {
-    Debug.info(`Gifted ${VERSION}`)
-  try {
-    await bot.init()
-    bot.logger.info('⏳ Gifted Database is syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
-    start();
-  }
-}
-start();
 
 
 
