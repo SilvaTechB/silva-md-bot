cc45d3244894429fc9008388b5a56b1fbaa35d5a
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-14 02:24:33 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

---
+++ plugins/updates.js
@@ -0,0 +1,114 @@
+/**
+//════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                            //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
+//                                                                                            // 
+//                                         Ｖ：2．5．0                                         // 
+//                                                                                            // 
+//                                                                                            // 
+//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
+//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
+//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
+//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
+//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
+//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//════════════════════════════════════════════════════════════════════════════════════════════//
+*                                                                 
+  * @project_name : Gifted-Md
+   * @author : Gifted Tech.
+   * @youtube : https://www.youtube.com/@giftedtechnexus
+   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @version 2.5.0
+*
+* 
+   * Created By Gifted Tech.
+   * © 2024 Gifted-Md.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let { smd, smdBuffer} = require(global.lib_dir || "../lib");
+let fs = require("fs");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const Config = require("../config");
+const { react } = require("../lib/gifted");
+let s_ser = true;
+smd(
+  {
+    pattern: "wachannel",
+    desc: "To check ping",
+    react: "🗨️",
+    category: "user",
+    filename: __filename,
+  },
+  async (message) => {
+    const channelMessage = `*GIFTED MD SUPPORT CHANNEL*\n\n _ʜᴇʏ ʜᴇʀᴇ's ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ, ᴘʟᴇᴀsᴇ ғᴏʟʟᴏᴡ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ ᴜs ᴛᴏ ᴋᴇᴇᴘ ᴛʜɪs ᴘʀᴏᴊᴇᴄᴛ ᴀʟɪᴠᴇ_\n *ʟɪɴᴋ:* https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o\n\n ${Config.botname} *WORKS*`;
+
+    const contextInfo = {
+      forwardingScore: 999,
+      isForwarded: true,
+    };
+
+    await message.send(channelMessage, { contextInfo });
+  }
+);
+smd(
+  {
+    pattern: "wagroup",
+    desc: "To check ping",
+    react: "🗨️",
+    category: "user",
+    filename: __filename,
+  },
+  async (message) => {
+    const SupportMsg = `*GIFTED MD SUPPORT GROUP*n\n *ʟɪɴᴋ:* https://chat.whatsapp.com/Cv3dRoX0q1dAfKs7OOkJTW\n\n ${Config.botname} *WORKS*`;
+
+    const contextInfo = {
+      forwardingScore: 999,
+      isForwarded: true,
+    };
+
+    await message.send(SupportMsg, { contextInfo });
+  }
+);
+smd(
+  {
+    pattern: "wadiscussion",
+    desc: "To check ping",
+    react: "🗨️",
+    category: "user",
+    filename: __filename,
+  },
+  async (message) => {
+    const SupportMsg = `*GIFTED MD DISCUSSION GROUP*n\n *ʟɪɴᴋ:* https://chat.whatsapp.com/KuKmv83j2Zw4zBuq7bbxBP\n\n ${Config.botname} *WORKS*`;
+
+    const contextInfo = {
+      forwardingScore: 999,
+      isForwarded: true,
+    };
+
+    await message.send(SupportMsg, { contextInfo });
+  }
+);
