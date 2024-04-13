a0b0a6ddac8b109947bcdb6818166a96ce5e17e6
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-13 12:40:26 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

---
+++ plugins/bible.js
@@ -0,0 +1,86 @@
+/**
+//════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                            //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
+//                                                                                            // 
+//                                         Ｖ：2．5．0                                         // 
+//                                                                                            // 
+//                                                                                            // 
+//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
+//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
+//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
+//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
+//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
+//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//════════════════════════════════════════════════════════════════════════════════════════════//
+*                                                                 
+  * @project_name : Gifted-Md
+   * @author : Gifted Tech.
+   * @youtube : https://www.youtube.com/@giftedtechnexus
+   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @version 2.5.0
+*
+* 
+   * Created By Gifted Tech.
+   * © 2024 Gifted-Md.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x83010e=_0x3126;(function(_0x4a7393,_0x1b3151){const _0x147611=_0x3126,_0x5e5e11=_0x4a7393();while(!![]){try{const _0x2d2f84=-parseInt(_0x147611(0x13b))/0x1+parseInt(_0x147611(0x13e))/0x2+parseInt(_0x147611(0x122))/0x3+parseInt(_0x147611(0x123))/0x4+-parseInt(_0x147611(0x132))/0x5+-parseInt(_0x147611(0x12c))/0x6+-parseInt(_0x147611(0x134))/0x7;if(_0x2d2f84===_0x1b3151)break;else _0x5e5e11['push'](_0x5e5e11['shift']());}catch(_0x2c8b36){_0x5e5e11['push'](_0x5e5e11['shift']());}}}(_0x5b02,0xcc20b));import _0x5690ed from'node-fetch';function _0x5b02(){const _0x5c1180=['join','slice','command','split','272634tmgVuE','tags','\x0a\x0aNumber\x20of\x20verses:\x20','2241324hLOwxK','length','error','religion','*\x0a\x0aType:\x20','3635862pZDuxK','1453168MgnvoO','chapter','reply','text','\x0a\x0a🔮\x20*Chapter\x20Content\x20(English):*\x0a\x0a','help','trim','json','bible\x20[chapter_number|chapter_name]','2008644jDnuYc','message','\x0a\x0a🔮\x20*Chapter\x20Content\x20(Hindi):*\x0a\x0a','reference','translation_name','verses','1072485IeiCdW','Error:\x20','7265272pmkIfq','Please\x20specify\x20the\x20chapter\x20number\x20or\x20name.\x20Example:\x20-bible\x20john\x203:16','\x0a📖\x20*The\x20Holy\x20Bible*\x0a\x0a📜\x20*Chapter\x20'];_0x5b02=function(){return _0x5c1180;};return _0x5b02();}import{translate}from'@vitalets/google-translate-api';const BASE_URL='https://bible-api.com';let bibleChapterHandler=async(_0x1385a6,{conn:_0x293dda})=>{const _0x101859=_0x3126;try{let _0x5d693d=_0x1385a6[_0x101859(0x126)][_0x101859(0x13a)]('\x20')[_0x101859(0x138)](0x1)[_0x101859(0x137)]('')[_0x101859(0x129)]();if(!_0x5d693d)throw new Error(_0x101859(0x135));_0x5d693d=encodeURIComponent(_0x5d693d);let _0x30a457=await _0x5690ed(BASE_URL+'/'+_0x5d693d);if(!_0x30a457['ok'])throw new Error(_0x101859(0x135));let _0x2fc925=await _0x30a457[_0x101859(0x12a)](),_0x383e67=await translate(_0x2fc925[_0x101859(0x126)],{'to':'hi','autoCorrect':!![]}),_0x736f63=await translate(_0x2fc925['text'],{'to':'en','autoCorrect':!![]}),_0x155828=_0x101859(0x136)+_0x2fc925[_0x101859(0x12f)]+_0x101859(0x121)+_0x2fc925[_0x101859(0x130)]+_0x101859(0x13d)+_0x2fc925[_0x101859(0x131)][_0x101859(0x13f)]+_0x101859(0x127)+_0x736f63['text']+_0x101859(0x12e)+_0x383e67[_0x101859(0x126)];_0x1385a6[_0x101859(0x125)](_0x155828);}catch(_0x329b09){console[_0x101859(0x140)](_0x329b09),_0x1385a6[_0x101859(0x125)](_0x101859(0x133)+_0x329b09[_0x101859(0x12d)]);}};bibleChapterHandler[_0x83010e(0x128)]=[_0x83010e(0x12b)],bibleChapterHandler[_0x83010e(0x13c)]=[_0x83010e(0x141)],bibleChapterHandler[_0x83010e(0x139)]=['bible',_0x83010e(0x124)];function _0x3126(_0x2134ad,_0x21d0f6){const _0x5b0209=_0x5b02();return _0x3126=function(_0x31267,_0x48428a){_0x31267=_0x31267-0x121;let _0x3e7250=_0x5b0209[_0x31267];return _0x3e7250;},_0x3126(_0x2134ad,_0x21d0f6);}export default bibleChapterHandler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
