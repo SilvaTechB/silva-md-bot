d55bb3670de8246141407d1326fe85573cad0334
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-13 13:56:48 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

---
+++ plugins/emojimix.js
@@ -0,0 +1,67 @@
+/**
+//════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                            //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
+//                                                                                            // 
+//                                         Ｖ：2．5．0                                         // 
+//                                                                                            // 
+//                                                                                            // 
+//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
+//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
+//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
+//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
+//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
+//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//════════════════════════════════════════════════════════════════════════════════════════════//
+*                                                                 
+  * @project_name : Gifted-Md
+   * @author : Gifted Tech.
+   * @youtube : https://www.youtube.com/@giftedtechnexus
+   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @version 2.5.0
+*
+* 
+   * Created By Gifted Tech.
+   * © 2024 Gifted-Md.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x40c8ef=_0x3bbf;(function(_0x47d7db,_0x4a98c9){const _0x39ac5c=_0x3bbf,_0x21eec5=_0x47d7db();while(!![]){try{const _0x31fa26=-parseInt(_0x39ac5c(0x1bc))/0x1*(-parseInt(_0x39ac5c(0x1b2))/0x2)+parseInt(_0x39ac5c(0x1ac))/0x3+parseInt(_0x39ac5c(0x1ae))/0x4*(-parseInt(_0x39ac5c(0x1b6))/0x5)+parseInt(_0x39ac5c(0x1bf))/0x6+parseInt(_0x39ac5c(0x1be))/0x7*(-parseInt(_0x39ac5c(0x1ad))/0x8)+-parseInt(_0x39ac5c(0x1a6))/0x9+-parseInt(_0x39ac5c(0x1b5))/0xa*(-parseInt(_0x39ac5c(0x1b7))/0xb);if(_0x31fa26===_0x4a98c9)break;else _0x21eec5['push'](_0x21eec5['shift']());}catch(_0x1f05de){_0x21eec5['push'](_0x21eec5['shift']());}}}(_0x169f,0xf25b7));import _0x43a4c3 from'node-fetch';import{sticker}from'../lib/sticker.js';const fetchJson=(_0x3490aa,_0x1d35d6)=>new Promise(async(_0x131c27,_0x363ac3)=>{const _0x49a7c0=_0x3bbf;_0x43a4c3(_0x3490aa,_0x1d35d6)[_0x49a7c0(0x1ab)](_0xe61567=>_0xe61567['json']())[_0x49a7c0(0x1ab)](_0x2eb753=>{_0x131c27(_0x2eb753);})[_0x49a7c0(0x1a7)](_0x56e17e=>{_0x363ac3(_0x56e17e);});});let handler=async(_0x295f87,{conn:_0x2373fb,text:_0x1ab775,args:_0x25861a,usedPrefix:_0x492f90,command:_0x5e5ed1})=>{const _0x1d88d3=_0x3bbf;if(!_0x25861a[0x0])throw'📌\x20Example\x20:\x20'+(_0x492f90+_0x5e5ed1)+_0x1d88d3(0x1ba);if(!_0x1ab775[_0x1d88d3(0x1a9)]('+'))throw _0x1d88d3(0x1a8)+(_0x492f90+_0x5e5ed1)+_0x1d88d3(0x1b3);let [_0x50d92e,_0x1f4cb6]=_0x1ab775['split']`+`,_0x594f1b=await fetchJson(_0x1d88d3(0x1bd)+encodeURIComponent(_0x50d92e)+'_'+encodeURIComponent(_0x1f4cb6));for(let _0x1a6b4d of _0x594f1b[_0x1d88d3(0x1b1)]){let _0x5c56da=await sticker(![],_0x1a6b4d[_0x1d88d3(0x1c1)],global[_0x1d88d3(0x1b9)],global[_0x1d88d3(0x1b0)]);_0x2373fb['sendFile'](_0x295f87[_0x1d88d3(0x1af)],_0x5c56da,null,{'asSticker':!![]},_0x295f87);}};handler[_0x40c8ef(0x1bb)]=['emojimix\x20<emoji+emoji>'],handler[_0x40c8ef(0x1a5)]=[_0x40c8ef(0x1b4)],handler[_0x40c8ef(0x1b8)]=[_0x40c8ef(0x1aa)],handler[_0x40c8ef(0x1c0)]=!![];export default handler;function _0x3bbf(_0x1c98d7,_0x1277b4){const _0x169fae=_0x169f();return _0x3bbf=function(_0x3bbfe5,_0x33bbad){_0x3bbfe5=_0x3bbfe5-0x1a5;let _0x353c55=_0x169fae[_0x3bbfe5];return _0x353c55;},_0x3bbf(_0x1c98d7,_0x1277b4);}function _0x169f(){const _0x5f58f6=['5zbELVS','https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=','19446jlcXNm','9472926KVoSpP','diamond','url','tags','17531028xgFtXe','catch','✳️\x20Separate\x20the\x20emoji\x20with\x20a\x20*+*\x20\x0a\x0a📌\x20Example\x20:\x20\x0a*','includes','emojimix','then','477195STWbYg','5424FSDXRI','16LhGMQv','chat','author','results','356894iBaZAu','*\x20😎+🤑','sticker','7812060ARjYgX','187085ajXrAA','33TWiFAb','command','packname','\x20😎+🤑','help'];_0x169f=function(){return _0x5f58f6;};return _0x169f();}
+
+
+
+
+
+
+
+
+
+
+
+
