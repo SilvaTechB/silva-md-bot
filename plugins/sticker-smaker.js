436d0d27b829999ed7d19854008ed4118dc5e1f6
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-13 14:02:39 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

--- plugins/sticker-smaker.js
+++ plugins/sticker-smaker.js
@@ -1,38 +1,62 @@
-import uploadImage from '../lib/uploadImage.js'
-import { sticker } from '../lib/sticker.js'
+/**
+//════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                            //
+//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                          //
+//                                                                                            // 
+//                                         Ｖ：2．5．0                                         // 
+//                                                                                            // 
+//                                                                                            // 
+//          ██████╗ ██╗███████╗████████╗███████╗██████╗     ███╗   ███╗██████╗                // 
+//         ██╔════╝ ██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██╔══██╗               // 
+//         ██║  ███╗██║█████╗     ██║   █████╗  ██║  ██║    ██╔████╔██║██║  ██║               // 
+//         ██║   ██║██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██║╚██╔╝██║██║  ██║               // 
+//         ╚██████╔╝██║██║        ██║   ███████╗██████╔╝    ██║ ╚═╝ ██║██████╔╝               // 
+//          ╚═════╝ ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═╝     ╚═╝╚═════╝                //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//                                                                                            //
+//════════════════════════════════════════════════════════════════════════════════════════════//
+*                                                                 
+  * @project_name : Gifted-Md
+   * @author : Gifted Tech.
+   * @youtube : https://www.youtube.com/@giftedtechnexus
+   * @description : Gifted-Md ,A Multi-functional whatsapp user bot.
+   * @version 2.5.0
+*
+* 
+   * Created By Gifted Tech.
+   * © 2024 Gifted-Md.
 
-const effects = ['jail', 'gay', 'glass', 'wasted' ,'triggered', 'lolice', 'simpcard', 'horny']
 
-let handler = async (m, { conn, usedPrefix, text, command }) => {
-let effect = text.trim().toLowerCase()
-if (!effects.includes(effect)) throw `
+*/
 
-┌─⊷ *EFFECTS*
-${effects.map(effect => `▢ ${effect}`).join('\n')}
-└───────────
 
-📌 *Example:* 
-${usedPrefix + command} wasted 
-`.trim()
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '✳️ Respond to an image'
-if (!/image\/(jpe?g|png)/.test(mime)) throw `✳️ Format not supported`
-let img = await q.download()
-let url = await uploadImage(img)
-let apiUrl = global.API('https://some-random-api.com/canvas/', encodeURIComponent(effect), {
-avatar: url
-})
-try {
-let stiker = await sticker(null, apiUrl, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-} catch (e) {
-m.reply('Conversion to sticker error, send as image instead')
-await conn.sendFile(m.chat, apiUrl, 'smaker.png', null, m)
-}}
-handler.help = ['smaker']
-handler.tags = ['sticker']
-handler.command = ['stickmaker', 'stickermaker', 'smaker'] 
-handler.diamond = false
 
-export default handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const _0x10d1e7=_0x1366;(function(_0x129902,_0x5713d9){const _0x2209c3=_0x1366,_0x590341=_0x129902();while(!![]){try{const _0x38a053=-parseInt(_0x2209c3(0xd9))/0x1*(parseInt(_0x2209c3(0xd8))/0x2)+-parseInt(_0x2209c3(0xb4))/0x3*(parseInt(_0x2209c3(0xc8))/0x4)+parseInt(_0x2209c3(0xda))/0x5*(-parseInt(_0x2209c3(0xd0))/0x6)+parseInt(_0x2209c3(0xd1))/0x7*(parseInt(_0x2209c3(0xc4))/0x8)+-parseInt(_0x2209c3(0xc2))/0x9+parseInt(_0x2209c3(0xdb))/0xa+parseInt(_0x2209c3(0xb2))/0xb;if(_0x38a053===_0x5713d9)break;else _0x590341['push'](_0x590341['shift']());}catch(_0x4af3da){_0x590341['push'](_0x590341['shift']());}}}(_0x121a,0x186dc));function _0x1366(_0x16e086,_0x219896){const _0x121a02=_0x121a();return _0x1366=function(_0x1366ce,_0x3f0e0d){_0x1366ce=_0x1366ce-0xb1;let _0x4a837f=_0x121a02[_0x1366ce];return _0x4a837f;},_0x1366(_0x16e086,_0x219896);}import _0xb8f25e from'../lib/uploadImage.js';import{sticker}from'../lib/sticker.js';const effects=['jail',_0x10d1e7(0xd5),_0x10d1e7(0xc5),'wasted',_0x10d1e7(0xbc),_0x10d1e7(0xc6),_0x10d1e7(0xb7),_0x10d1e7(0xcd)];let handler=async(_0x2da101,{conn:_0x5a3541,usedPrefix:_0x294245,text:_0x397019,command:_0x46c6e4})=>{const _0x3f193a=_0x10d1e7;let _0xd83332=_0x397019[_0x3f193a(0xd2)]()[_0x3f193a(0xbd)]();if(!effects[_0x3f193a(0xc1)](_0xd83332))throw(_0x3f193a(0xc7)+effects[_0x3f193a(0xcb)](_0x303d57=>'▢\x20'+_0x303d57)['join']('\x0a')+_0x3f193a(0xc9)+(_0x294245+_0x46c6e4)+_0x3f193a(0xba))[_0x3f193a(0xd2)]();let _0x22e658=_0x2da101[_0x3f193a(0xb6)]?_0x2da101[_0x3f193a(0xb6)]:_0x2da101,_0x332e8f=(_0x22e658['msg']||_0x22e658)[_0x3f193a(0xbb)]||'';if(!_0x332e8f)throw _0x3f193a(0xb1);if(!/image\/(jpe?g|png)/[_0x3f193a(0xb3)](_0x332e8f))throw _0x3f193a(0xc3);let _0x475e8=await _0x22e658[_0x3f193a(0xce)](),_0x58b7c3=await _0xb8f25e(_0x475e8),_0x1c1827=global[_0x3f193a(0xd6)](_0x3f193a(0xb8),encodeURIComponent(_0xd83332),{'avatar':_0x58b7c3});try{let _0xf70803=await sticker(null,_0x1c1827,global['packname'],global[_0x3f193a(0xb5)]);_0x5a3541[_0x3f193a(0xb9)](_0x2da101[_0x3f193a(0xbe)],_0xf70803,null,{'asSticker':!![]},_0x2da101);}catch(_0x5de45a){_0x2da101[_0x3f193a(0xd4)](_0x3f193a(0xd7)),await _0x5a3541['sendFile'](_0x2da101[_0x3f193a(0xbe)],_0x1c1827,_0x3f193a(0xcf),null,_0x2da101);}};handler[_0x10d1e7(0xd3)]=[_0x10d1e7(0xcc)],handler['tags']=[_0x10d1e7(0xdc)],handler[_0x10d1e7(0xc0)]=[_0x10d1e7(0xbf),_0x10d1e7(0xca),_0x10d1e7(0xcc)],handler['diamond']=![];export default handler;function _0x121a(){const _0xf05727=['✳️\x20Respond\x20to\x20an\x20image','2874311nWwweU','test','103839lJNDPQ','author','quoted','simpcard','https://some-random-api.com/canvas/','sendFile','\x20wasted\x20\x0a','mimetype','triggered','toLowerCase','chat','stickmaker','command','includes','800622LCUEWS','✳️\x20Format\x20not\x20supported','706648agUoJf','glass','lolice','\x0a\x0a┌─⊷\x20*EFFECTS*\x0a','8JUttBS','\x0a└───────────\x0a\x0a📌\x20*Example:*\x20\x0a','stickermaker','map','smaker','horny','download','smaker.png','24630Iusbsa','7jrxouY','trim','help','reply','gay','API','Conversion\x20to\x20sticker\x20error,\x20send\x20as\x20image\x20instead','349772zkfhXH','1NebWiV','5hGePVC','876030TPYvpB','sticker'];_0x121a=function(){return _0xf05727;};return _0x121a();}
