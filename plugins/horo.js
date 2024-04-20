7a1a1a5838bdd079ef85c6a90c252a5f156ef430
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-04-20 17:49:56 +0300

    ©²⁰²⁴ ɢɪғᴛᴇᴅ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛs

---
+++ plugins/horo.js
@@ -0,0 +1,45 @@
+const moment = require('moment-timezone')
+const {fetchJson,cmd, tlang } = require('../lib')
+let gis = require("async-g-i-s");
+const axios = require('axios')
+const fetch = require('node-fetch')
+
+
+
+//---------------------------------------------------------------------------
+cmd({
+            pattern: "horo",
+            category: "search",
+            desc: "Gives horoscope info of user.",
+            use: '<sign>\n:Example: horo libra',
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply("Provide me a sign!")
+            try {
+                const URL = `https://aztro.sameerkumar.website/?sign=${text}&day=today`;
+                fetch(URL, {
+                        method: 'POST'
+                    })
+                    .then(response => response.json())
+                    .then(json => {
+                        const date = json.current_date;
+                        console.log(date);
+                        let textw = "";
+                        textw += `*🌟 Horoscope of  ${text}*\n\n`;
+                        textw += `*Current Date:* ${json.current_date}.\n`;
+                        textw += `*Sign:* ${text}.\n`;
+                        textw += `*Lucky Time:* ${json.lucky_time}.\n`;
+                        textw += `*Compatibility:* ${json.compatibility}.\n`;
+                        textw += `*Lucky Number:* ${json.lucky_number}.\n`;
+                        textw += `*Lucky Color:* ${json.color}.\n`;
+                        textw += `*Today Mood:* ${json.mood}.\n`;
+                        textw += `*Overall:* ${json.description}.\n`;
+                        citel.reply(textw)
+                    });
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    )
