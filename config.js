4eaedff3ecb7f58836cdf880d0ab64e730c64ed2
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 21:51:44 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- config.js
+++ config.js
@@ -6,34 +6,34 @@ import fs from 'fs'
 
 //OwnerShip
 global.owner = [
-  [process.env.OWNER_NUMBER || '255734980103', process.env.OWNER_NAME || 'lazack28', true],
-  ['255779679079', 'lazack 28', true]
+  [process.env.OWNER_NUMBER || '254743706010', process.env.OWNER_NAME || '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓', true],
+  ['254700143167', '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃', true]
 ]
 global.mods = []
 global.prems = []
 
-global.author = process.env.OWNER_NAME || '𝐋𝐀𝐙𝐀𝐂𝐊-𝐌𝐃 v 2'
-global.botname = process.env.BOT_NAME || '𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿'
+global.author = process.env.OWNER_NAME || '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
+global.botname = process.env.BOT_NAME || '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
 global.oname = author
 global.bname = botname
 
 //Extra Shortcuts
-global.smlink = process.env.SOCIAL_MEDIA_LINK || 'https://instagram.com/Lazack_28'
-global.gclink = process.env.GROUP_LINK || 'https://chat.whatsapp.com/IIpL6gf6dcq4ial8gaJLE9'
+global.smlink = process.env.SOCIAL_MEDIA_LINK || 'https://github.com/SilvaTechB'
+global.gclink = process.env.GROUP_LINK || 'https://chat.whatsapp.com/Jjj2lYrtGHc5WY2rUmC6JD'
  
 //Apikeys
 global.shizokeys = 'shizo'
 
 //Sticker Watermarks
-global.packname = process.env.BOT_NAME || '𝐋𝐀𝐙𝐀𝐂𝐊-𝐌𝐃 🥵'
-global.stkpack = process.env.BOT_NAME || '𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿 🥵'
-global.stkowner = process.env.OWNER_NAME || '© lazack md v2'
+global.packname = process.env.BOT_NAME || '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
+global.stkpack = process.env.BOT_NAME || '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
+global.stkowner = process.env.OWNER_NAME || '© 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
 
 //Watermark
-global.maker = process.env.MAKER || 'Made with Lazack'
+global.maker = process.env.MAKER || 'Made with 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓'
 
 //global emojis
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ _𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
@@ -42,11 +42,11 @@ global.xmoji = '🔥'
 
 //management
 global.bug = '*!! Sorry 💢 !!*\nSomething went wrong 🌋'
-global.stop = '*!! 🎭 Unfortunately 💔 !!*\nBot system is not Responding 🙃'
+global.stop = '*!! 🎭 Unfortunately 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓💔 !!*\nBot system is not Responding 🙃'
 
 //TimeLines
-global.botdate = `*⫹⫺ Date:*  ${moment.tz('Asia/Kolkata').format('DD/MM/YY')}`
-global.bottime = `*⫹⫺ Time:* ${moment.tz('Asia/Kolkata').format('HH:mm:ss')}`
+global.botdate = `*⫹⫺ Date:*  ${moment.tz('Africa/Nairobi').format('DD/MM/YY')}`
+global.bottime = `*⫹⫺ Time:* ${moment.tz('Africa/Nairobi').format('HH:mm:ss')}`
 
 //Hosting Management
 global.serverHost = 1
@@ -56,9 +56,9 @@ global.replitHost = 0
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['255734980103','255734980103']
-global.prems = ['255734980103','255734980103']
-global.allowed = ['255734980103','255734980103']
+global.mods = ['254743706010','254700143167']
+global.prems = ['254700143167','254743706010']
+global.allowed = ['254743706010','254700143167']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
