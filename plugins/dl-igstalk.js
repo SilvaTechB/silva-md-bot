3840a751b01b3a145d33bd49cbfebc16d19eeb25
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 17:13:42 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -1,10 +1,10 @@
 import fg from 'api-dylux'
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   if (!args[0])
-    throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} asli_guru69`
+    throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} -its.silva`
   let res = await fg.igStalk(args[0])
   let te = `
-┌──「 *STALKING* 
+┌──「 *𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 STALKING* 
 ▢ *🔖Number:* ${res.name} 
 ▢ *🔖Username:* ${res.username}
 ▢ *👥followers:* ${res.followersH}
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 ▢ *🏝️Posts:* ${res.postsH}
 
 ▢ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
-└────────────`
+└─────𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓───────`
 
   await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
 }
