cee03a49bcebf830353f903ca5875a80f73266f0
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 15:35:48 +0300

    Add files via upload

---
+++ lazack-modules.js
@@ -0,0 +1,8 @@
+//Shizo_modules importing system 
+import GIFBufferToVideoBuffer from './Lazack/giftobuffer.js'
+import displayLoadingScreen from './Lazack/loading.js'
+
+
+//Shizo_modules quick access system
+global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
+global.displayLoadingScreen = displayLoadingScreen
