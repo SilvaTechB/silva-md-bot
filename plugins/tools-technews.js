c1aeafd4fddbdef048301058e7aa68052305931e
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 21:28:40 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- plugins/tools-technews.js
+++ plugins/tools-technews.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
 
     let techNews = `•───── ୨❀୧ ─────•
     ❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active
-    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: GURU
+    ㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: 𝐒𝐈𝐋𝐕𝐀
     ☞ 𝑵𝑬𝑾𝑺: ${json.news}
       •───── ୨❀୧ ─────•
     `
