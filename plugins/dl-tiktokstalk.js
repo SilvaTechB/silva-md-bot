9c05d684748303db6e9c16e4d7929669180f553e
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 19:22:11 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, args }) => {
   if (!text) throw `✳️Enter the Username of a TikTok user `
   let res = await fg.ttStalk(args[0])
   let txt = `
-┌──「 *TIKTOK STALK* 
+┌──「 *𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 TIKTOK STALK* 
 ▢ *🔖Number:* ${res.name}
 ▢ *🔖Username:* ${res.username}
 ▢ *👥followers:* ${res.followers}
@@ -11,6 +11,7 @@ let handler = async (m, { conn, text, args }) => {
 ▢ *📌Desc:* ${res.desc}
 
 ▢ *🔗 Link* : https://tiktok.com/${res.username}
+💌 follow: https://tiktok.com/silva.edits6
 └────────────`
   await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
 }
