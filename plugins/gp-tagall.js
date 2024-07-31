04723a01985a61e37ef30613f24f0a2bc7640004
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 19:48:56 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
   let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
   m.reply(
-    `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` +
+    `▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 MENTIONS*\n` +
       users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` +
-      '\n━━━━━━━━━━𝐋𝐀𝐙𝐀𝐂𝐊-𝐌𝐃━━━━━━━━━━━',
+      '\n━━━━━━━━━━𝐒𝐈𝐋𝐕𝐀-𝐌𝐃━━━━━━━━━━━',
     null,
     {
       mentions: users,
