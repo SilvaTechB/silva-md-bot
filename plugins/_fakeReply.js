5309cf216907f9f51cf5a3d177865a1c86e70bb3
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 17:03:14 +0300

    silva md bot

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -8,16 +8,16 @@ handler.all = async function (m) {
 	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/RCMg1aL.jpg')
 	
 	//reply link wa
-   global.rpgc = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'support group', title: 'JOIN GROUP', body: 'support group', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://chat.whatsapp.com/DWqdPuQ0yFkKyf1SzZ0k9Y' }}} 
+   global.rpgc = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'support group', title: 'JOIN GROUP', body: 'support group', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://chat.whatsapp.com/Lr80ac3MKKIKGew8mFES2a' }}} 
 	
 	//reply link Instagram 
-    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'FOLLOW DEVELOPER', title: 'INSTAGRAM', body: 'Keep bot alive', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://instagram.com/shizo_the_techie' }}}
+    global.rpig = { contextInfo: { externalAdReply: { mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'FOLLOW DEVELOPER', title: 'INSTAGRAM', body: 'Keep bot alive', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://instagram.com/_its.silva' }}}
 	
 	//reply link yt
-    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'SUBSCRIBE : ERROR MODS YT', title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://youtube.com/@ERRORMODSYT' }}}
+    global.rpyt = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'SUBSCRIBE : ERROR MODS YT', title: 'YouTube', body: 'learn to create your own bots', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://youtube.com/@silvaedits254' }}}
 
 //reply link WhatsApp Channel
-    global.rpwp = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'Follow Channel', title: 'OREO-BOT CHANNEL', body: 'To Get Updates About OREO-BOT', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://whatsapp.com/channel/0029VaCkzkr3wtb1uYWiRz2o' }}}
+    global.rpwp = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: 'https://i.imgur.com/RCMg1aL.jpg', mediaType: 'VIDEO', description: 'Follow Channel', title: 'SILVA-BOT CHANNEL', body: 'To Get Updates About SILVA-BOT', thumbnailUrl: 'https://i.imgur.com/RCMg1aL.jpg', sourceUrl: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v' }}}
     
 } 
 export default handler
