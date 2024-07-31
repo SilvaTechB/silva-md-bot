b3783d3121283431699148e6e6a94caa084b04de
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 21:13:30 +0300

    silva md bot

--- plugins/shizo-Openai.js
+++ plugins/shizo-Openai.js
@@ -40,7 +40,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       let data = await response.json()
       let result = data.completion
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Guru322`]], m)
+      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Silva power', `.silva`]], null, [['Follow Me', `https://github.com/SilvaTechB`]], m)
       m.react(done)
     }
   } catch (error) {
