4047b22048af3a48a3793ef6eab37650c61b68f6
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-31 20:18:48 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- plugins/main-listprem.js
+++ plugins/main-listprem.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     .map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net')
     .filter(v => v != conn.user.jid)
   let teks =
-    `▢ *PREMIUM USERS*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
+    `▢ *PREMIUM USERS*\n──────𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓───────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
   m.reply(teks, null, { mentions: conn.parseMention(teks) })
 }
 handler.help = ['listprem']
