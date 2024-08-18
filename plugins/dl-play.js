cba042bc13a9ccfaf671fe4d46d94b8d9d36c629
Silva <123529015+Sylivanu@users.noreply.github.com>
2024-08-18 10:32:37 +0300

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,90 +1,71 @@
-import ytdl from '@distube/ytdl-core';
-import yts from 'youtube-yts';
-import fs from 'fs';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import os from 'os';
-import axios from 'axios';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import yts from 'yt-search'
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`
+  await m.reply(wait)
+  let search = await yts(text)
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
+  if (!search) throw 'Video Not Found, Try Another Title'
+  let { title, thumbnail, timestamp, views, ago, url } = vid
+  let wm = 'Downloading audio please wait'
 
-const streamPipeline = promisify(pipeline);
-
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `${usedPrefix}${command} Fairy tale`;
-  await m.react(rwait);
-
-  try {
-    const query = encodeURIComponent(text);
-    const response = await axios.get(`https://www.guruapi.tech/api/ytsearch?text=${query}`);
-    const result = response.data.results[0];
-
-    if (!result) throw 'Video Not Found, Try Another Title';
-
-    const { title, url, thumbnail } = result;
-
-    const captvid = '*FOLLOW 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 support CHANNEL, AN ERROR OCCURRED TRY AGAIN AFTER 3 MONTHS YOU HAVE BEEN SUSPENDED FOR THAT DURATION FOR NOT FOLLOWING INSTRUCTIONS. keep loving 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓*';
-    const sourceUrl = "https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v";
-
-    conn.reply(m.chat, captvid, m, {
-      contextInfo: {
-        externalAdReply: {
-          title: `𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓`,
-          thumbnailUrl: thumbnail,
-          sourceUrl,
-          mediaType: 1,
-          renderLargerThumbnail: false
-        }
-      }
-    });
-
-    const audioStream = ytdl(url, {
-      filter: 'audioonly',
-      quality: 'highestaudio',
-    });
-
-    const tmpDir = os.tmpdir();
-    const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
-    await streamPipeline(audioStream, writableStream);
-
-    const doc = {
-      audio: {
-        url: `${tmpDir}/${title}.mp3`
-      },
-      mimetype: 'audio/mpeg',
-      ptt: false,
-      waveform: [100, 0, 0, 0, 0, 0, 100],
-      fileName: `${title}`,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: title,
-          body: 'HERE IS YOUR SONG WITH 𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓',
-          sourceUrl: url,
-          thumbnail: await (await conn.getFile(thumbnail)).data
-        }
-      }
-    };
-
-    await conn.sendMessage(m.chat, doc, { quoted: m });
-
-    fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
-      if (err) {
-        console.error(`Failed to delete audio file: ${err}`);
-      } else {
-        console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    throw '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓\n\nSomething went wrong 🥺 please try again later';
+  let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ Title: ${title}
+⬡ Duration: ${timestamp}
+⬡ Views: ${views}
+⬡ Upload: ${ago}
+⬡ Link: ${url}
+╰────────⬣`
+  conn.sendButton(m.chat, `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ Title: ${title}
+⬡ Duration: ${timestamp}
+⬡ Views: ${views}
+⬡ Upload: ${ago}
+⬡ Link: ${url}
+╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['VIDEO', `${usedPrefix}ytmp4 ${url}`], false, { quoted: m, 'document': { 'url':'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v' },
+'mimetype': global.dpdf,
+'fileName': `𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓`,
+'fileLength': 666666666666666,
+'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: `${url}`,
+title: `AUDIO IS BEING SENT...`,
+body: wm,
+sourceUrl: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v', thumbnail: await ( await conn.getFile(thumbnail)).data
   }
-};
+ } 
+})
 
-handler.help = ['play'].map((v) => v + ' <query>');
-handler.tags = ['downloader'];
-handler.command = ['play', 'song']
+  //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
+ //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
 
-handler.exp = 0;
+  const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
+const link = await yt.audio['128kbps'].download()
+  let doc = { 
+  audio: 
+  { 
+    url: link 
+}, 
+mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: url,
+title: title,
+body: wm,
+sourceUrl: url,
+thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
+                                                                                                                 }
+                       }
+  }
 
-export default handler;
+  return conn.sendMessage(m.chat, doc, { quoted: m })
+        // return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
+        // return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
+}
+handler.help = ['play'].map(v => v + ' <query>')
+handler.tags = ['downloader']
+handler.command = /^play$/i
+
+handler.exp = 0
+handler.diamond = false
+
+export default handler
