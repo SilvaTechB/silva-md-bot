0a652fecf1b51d8380948d9a6c4411664ec70e3f
Silva <123529015+Sylivanu@users.noreply.github.com>
2024-08-20 23:30:42 +0300

    Update dl-spotify.js

--- plugins/dl-spotify.js
+++ plugins/dl-spotify.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text }) => {
   await displayLoadingScreen(conn, m.chat)
   let pp = 'https://wallpapercave.com/wp/wp7932387.jpg'
   const query = encodeURIComponent(text)
-  let res = `https://guruapi.tech/api/spotifydl?url=${query}`
+  let res = `https://api.giftedtechnexus.co.ke/api/download/spotifydl?url=${query}`
   // let spotify = await (await fetch(res)).buffer()
   let doc = {
     audio: {
