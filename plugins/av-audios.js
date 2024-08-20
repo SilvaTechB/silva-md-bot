941db5ae2f8f766f3985a56eeb3d67dbc1faf32d
Silva <123529015+Sylivanu@users.noreply.github.com>
2024-08-20 15:19:11 +0300

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -14,6 +14,7 @@ export default handler
 
 let audioMsg = {
   'silva': './media/Alive.mp3',
+  'menu': './media/Menu.mp3',
   'bot': 'https://j.top4top.io/m_26464yyei1.mp3',
   'hello': 'https://i.top4top.io/m_2646qxac91.mp3',
   'sasa': 'https://h.top4top.io/m_26460eg6v1.mp3',
