7fbd38082d58948eab1cd8918ba52410a9ae64cb
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-09-20 12:41:41 +0300

    fix 🕷️

--- lazackcmds/main-script.js
+++ lazackcmds/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path'
 import axios from 'axios'
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/Lazack28/Lazack-Device'
+  const githubRepoURL = 'https://github.com/SilvaTechB/silva-md-bot'
 
   try {
     const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/)
