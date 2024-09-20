3f9528c13db74cd567bf91e742d4dafee35e3e22
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-09-20 18:17:56 +0300

    fix💀

---
+++ lazackcmds/gpt.js
@@ -0,0 +1,55 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `Please provide some text or quote a message to get a response.`
+  }
+
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text
+  }
+
+  try {
+    m.react(rwait)
+    
+    conn.sendPresenceUpdate('composing', m.chat)
+    const prompt = encodeURIComponent(text)
+
+    const guru1 = `https://api.gurusensei.workers.dev/llama?prompt=${prompt}`
+
+    try {
+      let response = await fetch(guru1)
+      let data = await response.json()
+      let result = data.response.response
+
+      if (!result) {
+        throw new Error('No valid JSON response from the first API')
+      }
+
+      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Guru322`]], m)
+      m.react(done)
+    } catch (error) {
+      console.error('Error from the first API:', error)
+
+      //const model = 'llama'
+     // const senderNumber = m.sender.replace(/[^0-9]/g, '')
+      //const session = `GURU_BOT_${senderNumber}`
+      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`
+
+      let response = await fetch(guru2)
+      let data = await response.json()
+      let result = data.completion
+
+      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Silva power', `.silva`]], null, [['Follow Me', `https://github.com/SilvaTechB`]], m)
+      m.react(done)
+    }
+  } catch (error) {
+    console.error('Error:', error)
+    throw `*ERROR*`
+  }
+}
+handler.help = ['chatgpt']
+handler.tags = ['AI']
+handler.command = ['bro', 'chatgpt', 'ai', 'gpt']
+
+export default handler
