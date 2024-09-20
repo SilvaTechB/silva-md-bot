8690c6a14bb2a2bf9e28475e4012853d7d480f6f
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-09-20 11:26:55 +0300

    𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓

--- lazack-modules.js
+++
@@ -1,8 +0,0 @@
-//Shizo_modules importing system 
-import GIFBufferToVideoBuffer from './Lazack/giftobuffer.js'
-import displayLoadingScreen from './Lazack/loading.js'
-
-
-//Shizo_modules quick access system
-global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
-global.displayLoadingScreen = displayLoadingScreen
