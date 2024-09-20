6130ef9342d8b4a1359e26c5ac62a1d001ecacd0
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-09-20 22:32:38 +0300

    fix 🗿

--- lazackcmds/greetings.js
+++ lazackcmds/greetings.js
@@ -3,8 +3,11 @@ export async function all(m) {
   if (
     (m.mtype === 'hellomessage' ||
       m.text.startsWith('Hello') ||
-      m.text.startsWith('hi') ||
-      m.text.startsWith('mambo')) &&
+      m.text.startsWith('Hi') ||
+      m.text.startsWith('Mambo') ||
+      m.text.startsWith('Oy') ||
+      m.text.startsWith('Niaje') ||
+      m.text.startsWith('kaka')) &&
     !m.isBaileys &&
     !m.isGroup
  /* ) {
@@ -16,9 +19,9 @@ export async function all(m) {
       { quoted: m }*/
     ) {
     this.sendButton(m.chat, `*WELCOME ITS ME JUST REPLYING*      
-    morning or evening @${m.sender.split('@')[0]} 
+    Hello 💕🥰 @${m.sender.split('@')[0]} 
     i may be offline or i may be slow to respond you but wait i will be back soon 😇
-  `.trim(), igfg, null, [['OWNER HELP', '.menu']] , m, { mentions: [m.sender] })
+  `.trim(), igfg, null, [['OWNER HELP', '.grp'],['GET TEXT', '.repo']] , m, { mentions: [m.sender] })
     m.react('💕')
   }
 
