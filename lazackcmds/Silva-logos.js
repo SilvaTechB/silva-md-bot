ace2720fd3eb83e76bdac409bcb4c6a024950e3f
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-01-18 00:04:30 +0300

    🦋

--- lazackcmds/Silva-logos.js
+++ lazackcmds/Silva-logos.js
@@ -1,178 +1,139 @@
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-	
-   let tee = `✳️ ${mssg.notext}\n\n📌 ${mssg.example}: *${usedPrefix + command}* Ultra`
-   let too = `✳️ ${mssg.textSe} *+* \n\n📌 ${mssg.example}: \n*${usedPrefix + command}* Global *+* TechInfo`
-    m.react(rwait)
+let handler = async (message, { conn, args, text, usedPrefix, command }) => {
+  let errorMessage =
+    "✳️ " +
+    mssg.notext +
+    "\n\n📌 " +
+    mssg.example +
+    ": *" +
+    (usedPrefix + command) +
+    "* SILVA MD BOT";
+  message.react(rwait);
 
-switch (command) {
-	
-	case 'logololi':
-	if (!text) throw tee
-	let img = global.API('fgmods', '/api/maker/loli', { text }, 'apikey')
-	conn.sendFile(m.chat, img, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'neon': 
-	if (!text) throw tee
-	let ne = global.API('fgmods', '/api/textpro/neon', { text }, 'apikey')
-	conn.sendFile(m.chat, ne, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'devil': 
-	if (!text) throw tee
-	let de = global.API('fgmods', '/api/textpro/devil', { text }, 'apikey')
-	conn.sendFile(m.chat, de, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'transformer': 
-	if (!text) throw tee
-	let tra = global.API('fgmods', '/api/textpro/transformers', { text }, 'apikey')
-	conn.sendFile(m.chat, tra, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'thunder': 
-	if (!text) throw tee
-	let thu = global.API('fgmods', '/api/textpro/thunder', { text }, 'apikey')
-	conn.sendFile(m.chat, thu, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break  
-	case 'graffiti': 
-	if (!text) throw too
-	if (!text.includes('+')) throw too  
-	let [c, d] = text.split`+`
-	let gff = global.API('fgmods', '/api/textpro/graffiti', { text: c, text2: d}, 'apikey')
-	conn.sendFile(m.chat, gff, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'bpink': 
-	if (!text) throw tee
-	let bpin = global.API('fgmods', '/api/textpro/blackpink', { text }, 'apikey')
-	conn.sendFile(m.chat, bpin, 'logo.png', `✅${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'joker': 
-	if (!text) throw tee
-	let jok = global.API('fgmods', '/api/textpro/joker', { text }, 'apikey')
-	conn.sendFile(m.chat, jok, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'matrix': 
-	if (!text) throw tee
-	let ma = global.API('fgmods', '/api/textpro/matrix', { text }, 'apikey')
-	conn.sendFile(m.chat, ma, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'wolf': 
-   if (!text) throw tee
-   let wo = global.API('fgmods', '/api/textpro/logowolf', { text: 'FG98', text2: text}, 'apikey')
-	conn.sendFile(m.chat, wo, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'glow': 
-	if (!text) throw tee
-	let glo = global.API('fgmods', '/api/textpro/advancedglow', { text }, 'apikey')
-	conn.sendFile(m.chat, glo, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'phlogo': 
-	if (!text) throw too
-	if (!text.includes('+')) throw too  
-	let [a, b] = text.split`+`   
-	let ph = global.API('fgmods', '/api/textpro/pornhub', { text: a, text2: b}, 'apikey')
-	conn.sendFile(m.chat, ph, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'ballon': 
-	if (!text) throw tee
-	let ball = global.API('fgmods', '/api/textpro/ballon', { text }, 'apikey')
-	conn.sendFile(m.chat, ball, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'dmd': 
-	if (!text) throw tee
-	let dm = global.API('fgmods', '/api/textpro/diamond', { text }, 'apikey')
-	conn.sendFile(m.chat, dm, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'lightglow': 
-	if (!text) throw tee
-	let lglo = global.API('fgmods', '/api/textpro/lightglow', { text }, 'apikey')
-	conn.sendFile(m.chat, lglo, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'american': 
-	if (!text) throw tee
-	let am = global.API('fgmods', '/api/textpro/American-flag', { text }, 'apikey')
-	conn.sendFile(m.chat, am, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'halloween': 
-	if (!text) throw tee
-	let hall = global.API('fgmods', '/api/textpro/halloween', { text }, 'apikey')
-	conn.sendFile(m.chat, hall, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'green': 
-	if (!text) throw tee
-	let hgre = global.API('fgmods', '/api/textpro/green-horror', { text }, 'apikey')
-	conn.sendFile(m.chat, hgre, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	case 'glitch': 
-	if (!text) throw tee
-	let igli = global.API('fgmods', '/api/textpro/impressive-glitch', { text }, 'apikey')
-	conn.sendFile(m.chat, igli, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'paper': 
-	if (!text) throw tee
-	let pap = global.API('fgmods', '/api/textpro/art-paper-cut', { text }, 'apikey')
-	conn.sendFile(m.chat, pap, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'marvel': 
-	if (!text) throw too
-	if (!text.includes('+')) throw too  
-	let [e, f] = text.split`+`   
-	let marv = global.API('fgmods', '/api/textpro/marvel', { text: e, text2: f}, 'apikey')
-	conn.sendFile(m.chat, marv, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'ninja': 
-	if (!text) throw too
-	if (!text.includes('+')) throw too  
-	let [g, h] = text.split`+`   
-	let nin = global.API('fgmods', '/api/textpro/ninja', { text: g, text2: h}, 'apikey')
-	conn.sendFile(m.chat, nin, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'future': 
-	if (!text) throw tee
-	let futu = global.API('fgmods', '/api/textpro/futuristic', { text }, 'apikey')
-	conn.sendFile(m.chat, futu, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case '3dbox': 
-	if (!text) throw tee
-	let box = global.API('fgmods', '/api/textpro/3dboxtext', { text }, 'apikey')
-	conn.sendFile(m.chat, box, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break
-	case 'graffiti2': 
-	if (!text) throw too
-	if (!text.includes('+')) throw too  
-	let [i, j] = text.split`+`
-	let gff2 = global.API('fgmods', '/api/textpro/graffiti2', { text: i, text2: j}, 'apikey')
-	conn.sendFile(m.chat, gff2, 'logo.png', `✅ ${mssg.result}`, m)
-	m.react(done)
-	break 
-	default:
-} 
-} 
-handler.help = ['logololi', 'graffiti2', '3dbox', 'future', 'ninja', 'marvel', 'paper', 'glitch', 'halloween', 'green', 'american', 'neon', 'devil', 'wolf', 'phlogo', 'transformer', 'thunder', 'graffiti', 'bpink', 'joker', 'matrix', 'glow', 'ballon', 'dmd', 'lightglow']
-handler.tags = ['maker']
-handler.command = /^(logololi|graffiti2|3dbox|future|ninja|marvel|paper|glitch|neon|green|halloween|american|devil|wolf|phlogo|transformer|thunder|graffiti|bpink|joker|matrix|glow|ballon|dmd|lightglow)$/i
-handler.diamond = true
+  let apiUrl;
+  switch (command) {
+    case "papercut":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/papercut?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "logomaker":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/logomaker?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "cartoon":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/cartoonstyle?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "writetext":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/writetext?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "glossy":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/glossysilver?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "bpstyle":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/blackpinkstyle?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "pixelglitch":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/pixelglitch?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "advancedglow":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/advancedglow?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "lighteffect":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/lighteffect?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "texteffect":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/texteffect?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "galaxy":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/galaxy?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "beach":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/summerbeach?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    case "clouds":
+      if (!text) throw errorMessage;
+      apiUrl =
+        "https://api.giftedtech.web.id/api/ephoto360/effectclouds?apikey=gifted-md&text=" +
+        encodeURIComponent(text);
+      break;
+    default:
+      throw "Command not recognized.";
+  }
 
-export default handler
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    if (result.success && result.result && result.result.image_url) {
+      conn.sendFile(
+        message.chat,
+        result.result.image_url,
+        "logo.png",
+        "*𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 © SILVA MD BOT*",
+        message
+      );
+      message.react(done);
+    } else {
+      throw "Failed to generate the image. Please try again later.";
+    }
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    message.reply(
+      "An error occurred while fetching the image. Please try again later."
+    );
+  }
+};
+
+handler.help = [
+  "papercut",
+  "logomaker",
+  "bpstyle",
+  "writetext",
+  "glossy",
+  "cartoon",
+  "pixelglitch",
+  "advancedglow",
+  "lighteffect",
+  "texteffect",
+  "galaxy",
+  "beach",
+  "clouds",
+];
+handler.tags = ["maker"];
+handler.command = /^(papercut|logomaker|bpstyle|pixelglitch|advancedglow|lighteffect|texteffect|galaxy|writetext|glossy|cartoon|beach|clouds)$/i;
+
+export default handler;
