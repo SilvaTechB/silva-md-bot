eb01fe0251253f4bd247cc6d9a6cd67f82d46bab
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-02-13 15:35:00 +0300

    SILVA MD BOT

--- lazackcmds/plugin-install.js
+++ lazackcmds/plugin-install.js
@@ -26,7 +26,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
       const pluginName = file.filename
 
       // Construct the path to save the plugin
-      const pluginPath = path.join('plugins', `${pluginName}`)
+      const pluginPath = path.join('lazackcmds', `${pluginName}`)
 
       // Write the Gist file content to the plugin file
       await fs.promises.writeFile(pluginPath, file.content)
