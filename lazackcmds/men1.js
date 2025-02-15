6ef3e1e85cddfade36bcf6a54d7a664f8f8e4401
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-02-15 11:39:31 +0300

    Update men1.js

--- lazackcmds/men1.js
+++ lazackcmds/men1.js
@@ -25,49 +25,67 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     let msg = generateWAMessageFromContent(m.chat, {
         viewOnceMessage: {
-            message: {
-                interactiveMessage: proto.Message.InteractiveMessage.create({
-                    body: proto.Message.InteractiveMessage.Body.create({
-                        text: str
-                    }),
-                    footer: proto.Message.InteractiveMessage.Footer.create({
-                        text: "Choose an option below"
-                    }),
-                    header: proto.Message.InteractiveMessage.Header.create({
-                        ...(await prepareWAMessageMedia({ image: { url: './media/shizo.jpg' } }, { upload: conn.waUploadToServer })),
-                        title: null,
-                        subtitle: null,
-                        hasMediaAttachment: false
-                    }),
-                    nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                        buttons: [
-                            {
-                                "name": "quick_reply",
-                                "buttonParamsJson": JSON.stringify({
-                                    "display_text": "MENU 📜",
-                                    "id": `${usedPrefix}botmenu`
-                                })
-                            },
-                            {
-                                "name": "cta_url",
-                                "buttonParamsJson": JSON.stringify({
-                                    "display_text": "OWNER 🌟",
-                                    "url": "https://wa.me/message/254700143167"
-                                })
-                            },
-                            {
-                                "name": "cta_url",
-                                "buttonParamsJson": JSON.stringify({
-                                    "display_text": "SCRIPT 💕",
-                                    "url": "https://github.com/SilvaTechB/silva-md-bot"
-                                })
-                            }
-                        ],
-                    })
-                })
-            }
+          message: {
+            "messageContextInfo": {
+              "deviceListMetadata": {},
+              "deviceListMetadataVersion": 2
+            },
+            interactiveMessage: proto.Message.InteractiveMessage.create({
+              body: proto.Message.InteractiveMessage.Body.create({
+                text: "Fizzxy Dev"
+              }),
+              footer: proto.Message.InteractiveMessage.Footer.create({
+                text: "Bot"
+              }),
+              header: proto.Message.InteractiveMessage.Header.create({
+                title: "Igna",
+                subtitle: "test",
+                hasMediaAttachment: false
+              }),
+              nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+                buttons: [
+                  {
+                    "name": "single_select",
+                    "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\".menu\":\".play dj webito\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+                  },
+                  {
+                    "name": "cta_reply",
+                    "buttonParamsJson": "{\"display_text\":\"quick_reply\",\"id\":\"message\"}"
+                  },
+                  {
+                    "name": "cta_url",
+                    "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
+                  },
+                  {
+                    "name": "cta_call",
+                    "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
+                  },
+                  {
+                    "name": "cta_copy",
+                    "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
+                  },
+                  {
+                    "name": "cta_reminder",
+                    "buttonParamsJson": "{\"display_text\":\"Recordatorio\",\"id\":\"message\"}"
+                  },
+                  {
+                    "name": "cta_cancel_reminder",
+                    "buttonParamsJson": "{\"display_text\":\"cta_cancel_reminder\",\"id\":\"message\"}"
+                  },
+                  {
+                    "name": "address_message",
+                    "buttonParamsJson": "{\"display_text\":\"address_message\",\"id\":\"message\"}"
+                  },
+                  {
+                    "name": "send_location",
+                    "buttonParamsJson": ""
+                  }
+                ],
+              })
+            })
+          }
         }
-    }, {});
+      }, {});
 
     await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
 }
